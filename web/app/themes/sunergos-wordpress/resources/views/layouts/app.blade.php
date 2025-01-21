a45b8a5eb6c2d1e1400d6eb4324d560a117b6ba9
Greg Crowell <Greg4510@gmail.com>
2025-01-21 11:30:59 -0500

    add GTM script

--- web/app/themes/sunergos-wordpress/resources/views/layouts/app.blade.php
+++ web/app/themes/sunergos-wordpress/resources/views/layouts/app.blade.php
@@ -1,6 +1,13 @@
 <!doctype html>
 <html @php(language_attributes())>
   <head>
+    <!-- Google Tag Manager -->
+      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-TZDQBFXN');</script>
+    <!-- End Google Tag Manager -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @if(!empty(get_field('meta_title')))
@@ -19,6 +26,10 @@
   </head>
 
   <body @php(body_class('bg-seashell'))>
+    <!-- Google Tag Manager (noscript) -->
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TZDQBFXN"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     @php(wp_body_open())
 
     <div id="app">
